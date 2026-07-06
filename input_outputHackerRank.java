3d9f0e92e1a17c2ae6f216ee378425703425e1b8
Amisha-Mohanty03 <amishamohanty321@gmail.com>
2026-07-06 23:02:57 +0530

    Solve HackerRank Stdin and Stdout II .

---
+++ input_outputHackerRank.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class input_outputHackerRank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine(); // Consume the leftover newline
+        String s = sc.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + a);
+
+        sc.close();
+    }
+}
